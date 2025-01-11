7ba985014aee5298334398c83a5106b970ddc65b
Jerome Kaphael Ghartey <gharteyjerome@gmail.com>
2025-01-11 19:34:40 +0000

    install pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,4 +1,9 @@
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# A Puppet manifest that creates a file at /tmp/school with specific owner, group, permission and content
+
+file { '/tmp/school':
+ensure  => file,
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0744',
+content => 'I love Puppet',
 }
