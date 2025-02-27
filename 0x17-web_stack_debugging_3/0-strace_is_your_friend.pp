a3c1e38ed8cdf906f8f539b9828cab3a19998707
Jerome Kaphael Ghartey <gharteyjerome@gmail.com>
2025-02-27 00:35:30 +0000

    lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
-# fixes Apache 500 error by fixing typo in wordpress
-exec { 'fix typo':
-  onlyif  => 'test -e /var/www/html/wp-settings.php',
+# This Puppet manifest fixes an Apache 500 error by correcting a typo in the WordPress wp-settings.php file
+
+exec { 'fix_typo':
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
 }
